fd25740402a1fe43a1222fed423d9590ce214579
santironhacker <santi92.network@gmail.com>
2020-09-14 16:19:49 +0200

    Installed ionic for angular via npm. Added ion-searchbar template to the app-header.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AppHeaderComponent } from './components/app-header/app-header.component';
+import { IonicModule } from '@ionic/angular';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { AppHeaderComponent } from './components/app-header/app-header.component
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    IonicModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent],
