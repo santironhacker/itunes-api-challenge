1a070a4b719fe1561093633a91f22bb1df9e0598
santironhacker <santi92.network@gmail.com>
2020-09-16 06:22:51 +0200

    Added empty path and songs-by-artist routes. Restructured the router-outlet placement to display the views inside the main tag.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SongsByArtistComponent } from './views/songs-by-artist/songs-by-artist.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/songs-by-artist', pathMatch: 'full' },
+  { path: 'songs-by-artist', component: SongsByArtistComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
