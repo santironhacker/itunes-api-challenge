330fe3e76002ff8d09968648dd2e383eb4dc5a72
santironhacker <santi92.network@gmail.com>
2020-09-17 11:04:59 +0200

    Added stylesheet to app-card component. Made cards responsive. Created a new mixin for media queries. Redefined grid breakpoints for songsByArtist component.

--- src/app/components/app-card/app-card.component.ts
+++ src/app/components/app-card/app-card.component.ts
@@ -3,7 +3,8 @@ import { ItunesMusicData } from 'src/app/models/itunes-music-data.model';
 
 @Component({
     selector: 'app-card',
-    templateUrl: './app-card.component.html'
+    templateUrl: './app-card.component.html',
+    styleUrls: ['./app-card.component.sass']
 })
 
 export class AppCardComponent {
