1f6dac5476867a1935b4bc51150ca3b4032cd9e0
santironhacker <santi92.network@gmail.com>
2020-09-18 06:35:45 +0200

    Added a subscription in the songsByArtist component to listen to the changes in results from the itunes-data service.

--- src/app/views/songs-by-artist/songs-by-artist.component.ts
+++ src/app/views/songs-by-artist/songs-by-artist.component.ts
@@ -1,64 +1,29 @@
-import { Component } from '@angular/core';
-import {  ItunesMusicData } from '../../models/itunes-music-data.model';
+import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { ItunesDataService } from 'src/app/services/itunes-data.service';
+import { ItunesMusicData } from '../../models/itunes-music-data.model';
 
 @Component({
     selector: 'songs-by-artist',
     templateUrl: './songs-by-artist.component.html'
 })
 
-export class SongsByArtistComponent {
-    public songsByArtist: ItunesMusicData[] = [
-        {
-            trackId: 528437613,
-            artistName: "LINKIN PARK",
-            collectionName: "Hybrid Theory",
-            artistViewUrl: "https://itunes.apple.com/us/artist/linkin-park/id148662?uo=4",
-            collectionViewUrl: "https://itunes.apple.com/us/album/in-the-end/id528436018?i=528437613&uo=4",
-            artworkUrl30: "http://is5.mzstatic.com/image/thumb/Music/v4/a3/da/73/a3da7362-c9f7-a6a4-bd00-632776ac74c1/source/30x30bb.jpg",
-            artworkUrl60: "http://is5.mzstatic.com/image/thumb/Music/v4/a3/da/73/a3da7362-c9f7-a6a4-bd00-632776ac74c1/source/60x60bb.jpg",
-            artworkUrl100: "http://is5.mzstatic.com/image/thumb/Music/v4/a3/da/73/a3da7362-c9f7-a6a4-bd00-632776ac74c1/source/100x100bb.jpg"
-        },
-        {
-            trackId: 528437613,
-            artistName: "LINKIN PARK",
-            collectionName: "Hybrid Theory",
-            artistViewUrl: "https://itunes.apple.com/us/artist/linkin-park/id148662?uo=4",
-            collectionViewUrl: "https://itunes.apple.com/us/album/in-the-end/id528436018?i=528437613&uo=4",
-            artworkUrl30: "http://is5.mzstatic.com/image/thumb/Music/v4/a3/da/73/a3da7362-c9f7-a6a4-bd00-632776ac74c1/source/30x30bb.jpg",
-            artworkUrl60: "http://is5.mzstatic.com/image/thumb/Music/v4/a3/da/73/a3da7362-c9f7-a6a4-bd00-632776ac74c1/source/60x60bb.jpg",
-            artworkUrl100: "http://is5.mzstatic.com/image/thumb/Music/v4/a3/da/73/a3da7362-c9f7-a6a4-bd00-632776ac74c1/source/100x100bb.jpg"
-        },
-        {
-            trackId: 528437613,
-            artistName: "LINKIN PARK",
-            collectionName: "Hybrid Theory",
-            artistViewUrl: "https://itunes.apple.com/us/artist/linkin-park/id148662?uo=4",
-            collectionViewUrl: "https://itunes.apple.com/us/album/in-the-end/id528436018?i=528437613&uo=4",
-            artworkUrl30: "http://is5.mzstatic.com/image/thumb/Music/v4/a3/da/73/a3da7362-c9f7-a6a4-bd00-632776ac74c1/source/30x30bb.jpg",
-            artworkUrl60: "http://is5.mzstatic.com/image/thumb/Music/v4/a3/da/73/a3da7362-c9f7-a6a4-bd00-632776ac74c1/source/60x60bb.jpg",
-            artworkUrl100: "http://is5.mzstatic.com/image/thumb/Music/v4/a3/da/73/a3da7362-c9f7-a6a4-bd00-632776ac74c1/source/100x100bb.jpg"
-        },
-        {
-            trackId: 528437613,
-            artistName: '',
-            collectionName: null,
-            artistViewUrl: "https://itunes.apple.com/us/artist/linkin-park/id148662?uo=4",
-            collectionViewUrl: "https://itunes.apple.com/us/album/in-the-end/id528436018?i=528437613&uo=4",
-            artworkUrl30: "http://is5.mzstatic.com/image/thumb/Music/v4/a3/da/73/a3da7362-c9f7-a6a4-bd00-632776ac74c1/source/30x30bb.jpg",
-            artworkUrl60: "http://is5.mzstatic.com/image/thumb/Music/v4/a3/da/73/a3da7362-c9f7-a6a4-bd00-632776ac74c1/source/60x60bb.jpg",
-            artworkUrl100: "http://is5.mzstatic.com/image/thumb/Music/v4/a3/da/73/a3da7362-c9f7-a6a4-bd00-632776ac74c1/source/100x100bb.jpg"
-        },
-        {
-            trackId: 528437613,
-            artistName: "LINKIN PARK",
-            collectionName: "Hybrid Theory",
-            artistViewUrl: "https://itunes.apple.com/us/artist/linkin-park/id148662?uo=4",
-            collectionViewUrl: "https://itunes.apple.com/us/album/in-the-end/id528436018?i=528437613&uo=4",
-            artworkUrl30: "http://is5.mzstatic.com/image/thumb/Music/v4/a3/da/73/a3da7362-c9f7-a6a4-bd00-632776ac74c1/source/30x30bb.jpg",
-            artworkUrl60: "http://is5.mzstatic.com/image/thumb/Music/v4/a3/da/73/a3da7362-c9f7-a6a4-bd00-632776ac74c1/source/60x60bb.jpg",
-            artworkUrl100: "http://is5.mzstatic.com/image/thumb/Music/v4/a3/da/73/a3da7362-c9f7-a6a4-bd00-632776ac74c1/source/100x100bb.jpg"
-        }
-    ]
+export class SongsByArtistComponent implements OnInit {
+    private subscription: Subscription;
+    public songsByArtist: ItunesMusicData[] = []
 
-    constructor() {}
+    constructor(private itunesDataService: ItunesDataService) {}
+
+    ngOnInit() {
+        this.subscription = this.itunesDataService.itunesDataChanged
+        .subscribe(
+            (itunesMusicData: ItunesMusicData[]) => {
+                this.songsByArtist = itunesMusicData;
+            }
+        )
+    }
+
+    ngOnDestroy() {
+        this.subscription.unsubscribe();
+    }
 }
