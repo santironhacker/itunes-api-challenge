2c93f018c22b154b9e3d7e9564d08e6b46eca087
santironhacker <santi92.network@gmail.com>
2020-09-23 15:06:03 +0200

    Control if the trackId already exists in the FavoriteIdsCollection and toggle the heart counter and heart icon accordingly.

--- src/app/services/favorites-counter.service.ts
+++ src/app/services/favorites-counter.service.ts
@@ -1,4 +1,5 @@
 import { Subject } from 'rxjs';
+import { throwIfEmpty } from 'rxjs/operators';
 import { FavoritesCounterData } from '../models/favorites-counter-data.model';
 
 export class FavoritesCounterService {
@@ -9,10 +10,16 @@ export class FavoritesCounterService {
     constructor() {}
 
     addSongToFavourites(trackId: number) {
-        this.favoriteSongCounter += 1;
-        const trackObject = {};
-        trackObject[trackId] = true;
-        Object.assign(this.favoritesIdsCollection, trackObject);
+        if(this.favoritesIdsCollection[trackId]) {
+            this.favoriteSongCounter -= 1;
+            this.favoritesIdsCollection[trackId] = false;
+        } 
+        else {
+            this.favoriteSongCounter += 1;
+            const trackObject = {};
+            trackObject[trackId] = true;
+            Object.assign(this.favoritesIdsCollection, trackObject);
+        }
         this.favoriteSongsCounterChanged.next({
             counter: this.favoriteSongCounter,
             favoritesIds: this.favoritesIdsCollection
